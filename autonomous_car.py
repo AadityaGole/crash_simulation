82c7bd64f6196f679ebb4135afd43bb60da7078b
modaniharsh-iiitb <harsh.modani@iiitb.ac.in>
2025-05-01 02:48:53 +0530

    minor change to gitignore

--- autonomous_car.py
+++ autonomous_car.py
@@ -10,8 +10,8 @@ np.random.seed(42)
 # Define the Car class
 class AutonoumousCar(Car):
     def __init__(self, config, strictness):
-        self.time_trained = min(max(1 - strictness + random.gauss(0, 0.1), 0), 1)
-        self.driving_expertise = min(max(max(self.time_trained, 0.5*strictness) + random.gauss(0, 0.1), 0), 1)
+        self.time_trained = min(max(1 - 0.8 * strictness + random.gauss(0, 0.1), 0), 1)
+        self.driving_expertise = min(max(max(self.time_trained, 0.5 * strictness) + random.gauss(0, 0.1), 0), 1)
         self.reaction_time = min(max(random.gauss(0.2, 0.01), 0), 1)
         self.fatigue = 0
         self.decision_making = min(max(2 * (0.6 * self.driving_expertise * self.time_trained * max(random.gauss(1.1, 0.1), 0)) ** 0.5, 0), 1)
