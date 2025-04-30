d4f01f79337fcc56a3e07aad670a407d6ae1dff9
modaniharsh-iiitb <harsh.modani@iiitb.ac.in>
2025-04-30 23:19:03 +0530

    changes to autonomous car variable generation

--- autonomous_car.py
+++ autonomous_car.py
@@ -10,12 +10,12 @@ np.random.seed(42)
 # Define the Car class
 class AutonoumousCar(Car):
     def __init__(self, config, strictness):
-        time_trained = min(max(-strictness, 0), 1)
-        self.driving_expertise = min(max(max(time_trained, 0.5*strictness) + random.gauss(0, 0.1), 0), 1)
-        self.reaction_time = min(max(random.gauss(0.2, 0.1), 0), 1)
+        self.time_trained = min(max(1 - strictness + random.gauss(0, 0.1), 0), 1)
+        self.driving_expertise = min(max(max(self.time_trained, 0.5*strictness) + random.gauss(0, 0.1), 0), 1)
+        self.reaction_time = min(max(random.gauss(0.2, 0.01), 0), 1)
         self.fatigue = 0
-        self.decision_making = min(max(2 * (self.driving_expertise * time_trained * random.gauss(0.5, 1)) ** 0.5, 0), 1)
-        self.consistency = min(max(random.gauss(0.6, 0.1), 0), 1)
+        self.decision_making = min(max(2 * (0.6 * self.driving_expertise * self.time_trained * max(random.gauss(1.1, 0.1), 0)) ** 0.5, 0), 1)
+        self.consistency = min(max(1.2 * strictness + random.gauss(0, 0.1), 0), 1)
 
         super().__init__(
             driving_expertise=self.driving_expertise,
