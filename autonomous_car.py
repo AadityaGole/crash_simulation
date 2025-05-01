1f749713c50a3c07c75353e30c3035e7dde405d5
modaniharsh-iiitb <harsh.modani@iiitb.ac.in>
2025-05-01 22:34:11 +0530

    changes to human car variable generation

--- autonomous_car.py
+++ autonomous_car.py
@@ -14,8 +14,8 @@ class AutonoumousCar(Car):
         self.time_trained = min(max(1 - 0.7 * strictness + random.gauss(0, 0.04), 0), 1)
 
         self.driving_expertise = min(max(max(0.6 * math.tanh(2 * self.time_trained), 0.8 * strictness) + random.gauss(0, 0.04), 0), 1)
-        self.reaction_time = min(max(random.gauss(0.2, 0.04), 0), 1)
         self.fatigue = 0
+        self.reaction_time = min(max(random.gauss(0.2, 0.04), 0), 1)
         self.decision_making = min(max((0.6 * self.driving_expertise * self.time_trained * max(random.gauss(1.1, 0.01), 0)) ** 0.5, 0), 1)
         self.consistency = min(max(0.5 * strictness + math.tanh(self.time_trained * self.driving_expertise) + random.gauss(0, 0.1), 0), 1)
 
--- human_car.py
+++ human_car.py
@@ -2,19 +2,22 @@ from car import Car
 from mesa import Agent
 import random
 import numpy as np
+import math
 
 # Set random seed for reproducibility
 random.seed(42)
 np.random.seed(42)
 
 # Define the Car class
-class AutonoumousCar(Car):
-    def __init__(self, config=None):
-        self.driving_expertise = min(max(random.gauss(0.8, 0.1), 0), 1)
-        self.reaction_time = min(max(random.gauss(0.5, 0.1), 0), 1)
-        self.fatigue = min(max(random.gauss(0.2, 0.1), 0), 1)
-        self.decision_making = min(max(random.gauss(0.7, 0.1), 0), 1)
-        self.consistency = min(max(random.gauss(0.6, 0.1), 0), 1)
+class HumanCar(Car):
+    def __init__(self, config, strictness):
+        self.time_trained = random.gauss(0.5, 0.1)
+
+        self.driving_expertise = min(max(0.5 * math.tanh(2 * self.time_trained), 0.8 * strictness) + random.gauss(0, 0.04), 1)
+        self.fatigue = min(max(random.gauss(0.3, 0.1), 0), 1)
+        self.reaction_time = max(((1 + math.tanh(5 * self.fatigue))) * (1 - 0.2 * self.driving_expertise) / 2 + random.gauss(0, 0.04), 0.3)
+        self.decision_making = min(max(((1 - math.tanh(10 * self.fatigue))) * (self.time_trained) / 2 + random.gauss(0, 0.04), 0), 1)
+        self.consistency = min(max(0.5 + (self.time_trained * self.driving_expertise) / 2 + random.gauss(0, 0.1), 0), 1)
 
         super().__init__(
             driving_expertise=self.driving_expertise,
